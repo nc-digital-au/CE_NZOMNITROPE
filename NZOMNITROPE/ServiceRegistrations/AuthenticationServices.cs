c47d5d0197e5a88e226256b02006f3ba00f8d949
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2025-03-18 15:38:33 +1100

    fixed onitrope BFF set-up

--- NZOMNITROPE/ServiceRegistrations/AuthenticationServices.cs
+++ NZOMNITROPE/ServiceRegistrations/AuthenticationServices.cs
@@ -23,13 +23,13 @@ namespace NZOMNITROPE.ServiceRegistrations
             JsonWebTokenHandler.DefaultMapInboundClaims = false;
             services.AddAuthentication(options =>
             {
-                options.DefaultScheme = "ofev_cookie";
+                options.DefaultScheme = "omnitrope_cookie";
                 options.DefaultChallengeScheme = "oidc";
                 options.DefaultSignOutScheme = "oidc";
             })
-            .AddCookie("ofev_cookie", options =>
+            .AddCookie("omnitrope_cookie", options =>
             {
-                options.Cookie.Name = "__Host-ofev";
+                options.Cookie.Name = "__Host-omnitrope";
                 options.Cookie.SameSite = SameSiteMode.Strict;
             })
             .AddOpenIdConnect("oidc", options =>
@@ -44,6 +44,10 @@ namespace NZOMNITROPE.ServiceRegistrations
                 options.GetClaimsFromUserInfoEndpoint = true;
                 options.SaveTokens = true;
                 options.Scope.Clear();
+                 foreach (var scope in config.Scopes)
+                {
+                    options.Scope.Add(scope);
+                }
                 options.ClaimActions.Remove("aud");
                 options.ClaimActions.DeleteClaim("idp");
                 options.ClaimActions.MapJsonKey("role", "role");
@@ -52,11 +56,7 @@ namespace NZOMNITROPE.ServiceRegistrations
                 options.ClaimActions.MapJsonKey("prescriber_id", "prescriber_id");
                 options.ClaimActions.MapJsonKey("prescriber_number", "prescriber_number");
                 options.ClaimActions.MapJsonKey("ahpra_number", "ahpra_number");
-                
-                foreach (var scope in config.Scopes)
-                {
-                    options.Scope.Add(scope);
-                }
+                options.ClaimActions.MapJsonKey("registration_status", "registration_status");
 
                 options.TokenValidationParameters = new()
                 {
