89d9b2b1faf02cedeeb1a5a5922a91c2e0972b51
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2025-03-31 10:35:53 +1100

    added enviromnent to fix deployments

---
+++ NZOMNITROPE/ClientApp/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  programId: '2b8753ae-e823-4033-aef7-c4e102b463a1',
+  googleApiKey: 'AIzaSyCU-xrj6qLt4JijYTt9GPy8JJ1IyTTVpAY',
+  orderAdminEmail: 'testprogramadmin@nc-digital.com.au',
+  trainingAdminEmail: 'testprogramadmin@nc-digital.com.au'
+};
