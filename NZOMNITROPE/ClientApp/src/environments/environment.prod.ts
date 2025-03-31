f0aacd89233b398136697852a230393d0e8f71ad
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2025-03-31 15:18:18 +1100

    setting updates and production id

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  programId: '113ffa5c-196a-47d0-a0b1-10d759c73fb9',
+  programId: 'e97dc240-71e6-4a33-b28f-e540b650d167',
   googleApiKey: 'AIzaSyCU-xrj6qLt4JijYTt9GPy8JJ1IyTTVpAY',
   orderAdminEmail: 'omnipal.nz@sandoz.com',
   trainingAdminEmail: 'surepal.training_nz@sandoz.com'
