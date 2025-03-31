10c348936b9b916cca938bf758aa57bb9f9f2ed2
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2025-03-31 20:58:21 +1100

    appsettings update and environment

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   programId: '113ffa5c-196a-47d0-a0b1-10d759c73fb9',
   googleApiKey: 'AIzaSyCU-xrj6qLt4JijYTt9GPy8JJ1IyTTVpAY',
-  orderAdminEmail: 'omnipal.nz@sandoz.com',
-  trainingAdminEmail: 'surepal.training_nz@sandoz.com'
+  orderAdminEmail: 'testprogramadmin@nc-digital.com.au',
+  trainingAdminEmail: 'testprogramadmin@nc-digital.com.au'
 };
