94cc7e53090036ef8668de3bf5d16a7ae98cb89b
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2025-04-02 13:29:49 +1100

    added testing emails for admin

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   programId: '113ffa5c-196a-47d0-a0b1-10d759c73fb9',
   googleApiKey: 'AIzaSyCU-xrj6qLt4JijYTt9GPy8JJ1IyTTVpAY',
-  orderAdminEmail: 'testprogramadmin@nc-digital.com.au',
-  trainingAdminEmail: 'testprogramadmin@nc-digital.com.au'
+  orderAdminEmail: 'omnipal-order@nc-digital.com.au',
+  trainingAdminEmail: 'omnipal-booking@nc-digital.com.au'
 };
