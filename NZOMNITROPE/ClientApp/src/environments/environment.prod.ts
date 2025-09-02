b9c2db5a906b1fd336921adf3da9219c54ebec30
Marwin <marwin@nc-digital.com.au>
2025-09-02 12:01:12 +1000

    Added new google api key, changed it so that order cannot be submitted without selecting

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   programId: '113ffa5c-196a-47d0-a0b1-10d759c73fb9',
-  googleApiKey: 'AIzaSyCU-xrj6qLt4JijYTt9GPy8JJ1IyTTVpAY',
+  googleApiKey: 'AIzaSyA85_sTJVqFq-cldz0vpywUH-3DOrL1la0',
   orderAdminEmail: 'omnipal-order@nc-digital.com.au',
   trainingAdminEmail: 'omnipal-booking@nc-digital.com.au'
 };
