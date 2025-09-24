be50fc34636f1a87bf67fa8610e2cdbc402e9a5b
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2025-09-24 20:07:05 +1000

    Updated environments with correct google api key

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   programId: 'e97dc240-71e6-4a33-b28f-e540b650d167',
-  googleApiKey: 'AIzaSyCU-xrj6qLt4JijYTt9GPy8JJ1IyTTVpAY',
-  orderAdminEmail: 'omnipal-order@nc-digital.com.au',
-  trainingAdminEmail: 'omnipal-booking@nc-digital.com.au'
+  googleApiKey: 'AIzaSyA85_sTJVqFq-cldz0vpywUH-3DOrL1la0',
+  orderAdminEmail: 'omnipal.nz@sandoz.com',
+  trainingAdminEmail: 'surepal.training_nz@sandoz.com'
 };
