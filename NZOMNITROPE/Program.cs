039340d8d25409e7bb1e4e6dffbb2e6d9ec95713
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2025-10-12 18:40:08 +1100

    Update Program.cs

--- NZOMNITROPE/Program.cs
+++ NZOMNITROPE/Program.cs
@@ -29,20 +29,6 @@ if (!app.Environment.IsDevelopment())
 app.UseOidcProxy();
 app.UseRouting();
 
-// // Add API endpoints that forward tokens
-// app.MapPost("/api/{*path}", async (HttpContext context, IHttpClientFactory httpClientFactory) =>
-// {
-//     var accessToken = context.Request.Headers["Authorization"].FirstOrDefault()?.Replace("Bearer ", "");
-    
-//     var client = httpClientFactory.CreateClient();
-//     client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
-    
-//     var externalApiUrl = $"https://localhost:5144/api/{context.Request.RouteValues["path"]}";
-//     var response = await client.PostAsync(externalApiUrl, new StreamContent(context.Request.Body));
-    
-//     return Results.Stream(await response.Content.ReadAsStreamAsync(), response.Content.Headers.ContentType?.ToString());
-// });
-
 app.UseEndpoints(_ => { });
 
 app.UseSpa(spa =>
