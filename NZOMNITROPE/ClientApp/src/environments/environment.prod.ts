6c4bf99134ff7193eb4c550b11d4f99da5459f59
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2026-02-24 23:51:42 +1100

    Update environment.prod.ts

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: true,
-  programId: 'e97dc240-71e6-4a33-b28f-e540b650d167',
   googleApiKey: 'AIzaSyA85_sTJVqFq-cldz0vpywUH-3DOrL1la0',
   orderAdminEmail: 'omnipal.nz@sandoz.com',
   trainingAdminEmail: 'surepal.training_nz@sandoz.com',
-  tenantId: 'e97dc240-71e6-4a33-b28f-e540b650d167'
+  programId: 'e97dc240-71e6-4a33-b28f-e540b650d167',
+  keycloak: {
+    authority: 'https://app-keycloak-prod-ncd.azurewebsites.net/realms/Sandoz-Omnipal',
+    clientId: 'Omnipal-Portal',
+  }
 };
