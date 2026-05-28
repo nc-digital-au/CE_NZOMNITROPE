6af19e647101167e92efed6c2e79bd8f6042b462
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2026-05-28 21:07:49 +1000

    Added google analytics

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  googleAnalyticsId: 'G-QGJ8F9LT4N',
   programId: 'e97dc240-71e6-4a33-b28f-e540b650d167',
   googleApiKey: 'AIzaSyA85_sTJVqFq-cldz0vpywUH-3DOrL1la0',
   orderAdminEmail: 'omnipal.nz@sandoz.com',
