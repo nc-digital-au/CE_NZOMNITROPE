de69c8761322f0e39f3fa787209c840f58458297
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2026-09-01 20:47:09 +1000

    Update environment.prod.ts

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -5,7 +5,6 @@ export const environment = {
   googleApiKey: 'AIzaSyA85_sTJVqFq-cldz0vpywUH-3DOrL1la0',
   orderAdminEmail: 'omnipal.nz@sandoz.com',
   trainingAdminEmail: 'surepal.training_nz@sandoz.com',
-  programId: 'e97dc240-71e6-4a33-b28f-e540b650d167',
   keycloak: {
     authority: 'https://auth.patient-services.au/realms/Sandoz-Omnipal',
     clientId: 'Omnipal-Portal',
