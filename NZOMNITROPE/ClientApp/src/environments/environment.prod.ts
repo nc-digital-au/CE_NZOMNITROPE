930e6c9f78ac3d13a99bf4946a2bc4c385e910e8
nc-digital-au <145732753+nc-digital-au@users.noreply.github.com>
2026-09-01 20:42:00 +1000

    changed to new keycloak

--- NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
+++ NZOMNITROPE/ClientApp/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   trainingAdminEmail: 'surepal.training_nz@sandoz.com',
   programId: 'e97dc240-71e6-4a33-b28f-e540b650d167',
   keycloak: {
-    authority: 'https://app-keycloak-prod-ncd.azurewebsites.net/realms/Sandoz-Omnipal',
+    authority: 'https://auth.patient-services.au/realms/Sandoz-Omnipal',
     clientId: 'Omnipal-Portal',
   }
 };
